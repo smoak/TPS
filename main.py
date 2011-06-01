71991a037d6109ffcb3421641e38916b476759e7
Scott Moak <scott.moak@gmail.com>
2011-06-01 13:23:26 -0700

    Added some (basic) logging support. Continuing to evolve the code.

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import logging
+import logging.handlers
+
 from net.server import TerrariaServer
 
-if __name__ == '__main__':
+LOG_FILENAME = "main.log"
+
+log = logging.getLogger("main")
+
+def main():
+  # Add the log message handler to the logger
+  handler = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=10*1024*1024, backupCount=5)
+  formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+  handler.setFormatter(formatter)
+  TerrariaServer.log.addHandler(handler)
+  TerrariaServer.log.setLevel(logging.DEBUG)
   s = TerrariaServer("", 7777, None)
   s.start()
+
+if __name__ == '__main__':
+  main()
