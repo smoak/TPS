1b12695470e7f00a3667d4a52170c9f47f56dc22
Scott Moak <scott.moak@gmail.com>
2011-06-01 17:18:42 -0700

    Added some message types(still more to add).

---
+++ net/messagetype.py
@@ -0,0 +1,12 @@
+
+
+class MessageType:
+  ConnectionRequest = 0x01
+  Disconnect = 0x02
+  RequestPlayerData = 0x03
+  PlayerData = 0x04
+  InventoryData = 0x05
+  RequestWorldData = 0x06
+  WorldData = 0x07
+  TileBlockRequest = 0x08
+   
