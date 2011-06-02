021e9d4f7b4392f5b770c9ca8dc732f6ad1001a3
Sasha Moak <sasha.moak@gmail.com>
2011-06-01 17:02:22 -0700

    Basic connection management. Still huge WIP

--- net/server.py
+++ net/server.py
@@ -1,9 +1,12 @@
 import socket
 import thread
 import logging
+import select
+import struct
 
 from connectioninfo import ConnectionInfo
 
+HEADER_FORMAT = '<i' # little endian integer
 
 class NetworkState:
   Starting = 0
@@ -37,6 +40,24 @@ class ConnectionManager:
     self.__connections.remove(connection)
     self.locker.release()
 
+  def getListOfSocketsForSelect(self):
+    result = []
+    self.locker.acquire()
+    for ci in self.__connections:
+      result.append(ci.socket)
+    self.locker.release()
+    return result
+
+  def findConnection(self, socket):
+    result = None
+    self.locker.acquire()
+    for ci in self.__connections:
+      if ci.socket == socket:
+        result = ci
+        break
+    self.locker.release()
+    return result
+
 class TerrariaServer:
 
   log = logging.getLogger("TerrariaServer")
@@ -59,10 +80,26 @@ class TerrariaServer:
       self.log.error(ex)
       self.networkState = NetworkState.Error
 
+  def __doProtocol(self, connection):
+    self.log.debug("Got data from socket...doing protocol goodness")
+    try:
+      header = connection.socket.recv(4) # first 4 bytes tell us the message length
+      if len(header) == 0:
+        self.connectionManager.removeConnection(connection)
+      msgLen = struct.unpack(HEADER_FORMAT, header)[0] # unpack returns a tuple 
+      connection.data = connection.socket.recv(msgLen)
+      self.log.debug("Got message with " + str(msgLen) + " size")
+      # here is where we dispatch the message to a message handler to further process...
+      self.log.debug("Processing message...")
+    except Exception as ex:
+      self.log.error(ex)
+      self.connectionManager.removeConnection(connection)
+
   def __readThread(self):
     while self.networkState == NetworkState.Running:
-     # do fancy stuff here...
-     pass  
+      socketsToRead, socketsToWrite, socketsWithError = select.select(self.connectionManager.getListOfSocketsForSelect(), [], [], 5.0)
+      for s in socketsToRead:
+        self.__doProtocol(self.connectionManager.findConnection(s))
 
   def __mainLoop(self):
     while self.networkState == NetworkState.Running:
