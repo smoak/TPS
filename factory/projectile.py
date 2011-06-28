2ba7e3bed8b0fbd645af4be84ea5633960c97a12
Scott Moak <scott.moak@gmail.com>
2011-06-28 16:31:50 -0700

    Fixing some issues with projectiles. Still not quite working right...

--- factory/projectile.py
+++ factory/projectile.py
@@ -3,6 +3,8 @@ from service.projectile import ProjectileService
 class ProjectileFactory(object):
   def __init__(self):
     self.projectileService = ProjectileService()
+    # May want to break this out in a Manager class?
+    self.projecties = []
 	  
   def newProjectile(self, x, y, speedX, speedY, projectileType, damage, knockBack, owner = 255, wet = False):
     p = self.projectileService.getProjectileByType(projectileType)
@@ -13,5 +15,14 @@ class ProjectileFactory(object):
     p.damage = damage
     p.knockBack = knockBack
     p.owner = owner
-    p.wet = wet
+    p.wet = wet # Hmmm...this is really supposed to be some sort of weird collision detection thing..
+    p.identity = self.__nextAvailableNum()
+    self.projectiles.append(p)
+    return p
+
+  def __nextAvailableNum(self):
+    result = len(self.projectiles) + 1
+    if result >= 1000:
+      result = 999
+    return result 
     
